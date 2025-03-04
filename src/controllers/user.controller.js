b9ba821166267dc989ca6e14144e373320915514
krishnadas <krishnadasr2001@gmail.com>
2025-03-04 18:23:11 +0530

    add otp login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,15 @@
 import prisma from "../config/prismaClient.js";
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const userRegister = async (req, res) => {
 
   try {
       
-      const { firstName, lastName, email } = req.body;
+      const { firstName, lastName, email ,phone} = req.body;
 
       const existingUser = await prisma.user.findUnique({ where: { email } });
       
@@ -13,7 +18,7 @@ export const userRegister = async (req, res) => {
       }
   
       const user = await prisma.user.create({
-        data: { firstName, lastName, email},
+        data: { firstName, lastName, email,phone},
       });
   
       res.status(200).json({message:"user registered successfully",user});
@@ -24,3 +29,68 @@ export const userRegister = async (req, res) => {
     }
 };
 
+export const sendOtp = async(req,res)=>{
+
+  try{
+
+    const {phone} = req.body;
+    
+    if (!phone) {
+      return res.status(400).json({ error: "Phone number is required" });
+    }
+
+    const API_KEY = process.env.TWO_FACTOR_API_KEY
+
+
+    const response = await axios.get(
+      `https://2factor.in/API/V1/${API_KEY}/SMS/${phone}/AUTOGEN3`
+    );
+
+    if (response.data.Status !== "Success") {
+      return res.status(500).json({ error: "Failed to send OTP" });
+    }
+
+    res.json({ message: "OTP sent successfully", session_id: response.data.Details });
+
+  }
+
+  catch (error) {
+    console.log(error);
+    
+    res.status(500).json({ error: "Error sending OTP" });
+  }
+};
+
+export const verifyOtp = async (req, res) => {
+  try {
+    const { phone, otp } = req.body;
+
+    if (!phone || !otp) {
+      return res.status(400).json({ error: "Phone number and OTP are required" });
+    }
+
+    const API_KEY = process.env.TWO_FACTOR_API_KEY
+
+    const response = await axios.get(
+      `https://2factor.in/API/V1/${API_KEY}/SMS/VERIFY3/${phone}/${otp}`
+    );
+
+    if (response.data.Status !== "Success") {
+      return res.status(401).json({ error: "Invalid OTP" });
+    }
+
+    const JWT_SECRET = process.env.JWT_SECRET
+
+    const user = await prisma.user.findUnique({
+      where: { phone },
+    });
+
+    //  Generate JWT Token
+    const token = jwt.sign({ phone }, JWT_SECRET);
+
+    res.json({ message: "OTP verified successfully", token,exist:!!user });
+    
+  } catch (error) {
+    res.status(500).json({ error: "Error verifying OTP" });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import {userRegister} from '../controllers/user.controller.js';
+import {userRegister,sendOtp,verifyOtp} from '../controllers/user.controller.js';
 import {userSchema} from '../validators/index.js';
 import { validateRequest } from '../middleware/validateRequest.js';
 
 const router = express.Router();
 
 router.post('/register',validateRequest(userSchema),userRegister);
+router.post('/sendOtp',sendOtp);
+router.post('/verifyOtp',verifyOtp);
  
 
 export default router;
